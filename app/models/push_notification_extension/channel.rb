80dea05981a91fb473d198578058d2e15868aa4f
David Benko <DavidBenko@users.noreply.github.com>
2016-08-15 14:54:02 -0400

    Update channel.rb

--- app/models/push_notification_extension/channel.rb
+++ app/models/push_notification_extension/channel.rb
@@ -43,39 +43,34 @@ module PushNotificationExtension
         android_device_tokens << device.token if device.android?
       end
       
-      if Rails.env.production?
-        hashed_message_payload = Hash.new
-        begin
-          # Note that that app icons cannot be modified on the android side. This count will have to be displayed in 
-          # a widget or from the notification system.
-          hashed_message_payload["data"] = message_payload
-          hashed_message_payload["badge"] = badge
-        rescue
-          Rails.logger.error "Unable to parse the message payload for android: " + $!.message
-          Rails.logger.error $!.backtrace.join("\n")
-        end  
-        
-        unless hashed_message_payload.nil?
-          if AP::PushNotificationExtension::PushNotification.config[:gcm_api_key]
-            gcm = ::GCM.new(AP::PushNotificationExtension::PushNotification.config[:gcm_api_key])
-            if android_device_tokens
-              gcm_result = gcm.send_notification(android_device_tokens, data: hashed_message_payload)
-              Rails.logger.info "Channel #{self.name}: GCM push status: #{gcm_result}"
-            else
-              Rails.logger.info "Channel #{self.name}: There are no device tokens available for GCM."
-            end
+      hashed_message_payload = Hash.new
+      begin
+        # Note that that app icons cannot be modified on the android side. This count will have to be displayed in 
+        # a widget or from the notification system.
+        hashed_message_payload["data"] = message_payload
+        hashed_message_payload["badge"] = badge
+      rescue
+        Rails.logger.error "Unable to parse the message payload for android: " + $!.message
+        Rails.logger.error $!.backtrace.join("\n")
+      end  
+      
+      unless hashed_message_payload.nil?
+        if AP::PushNotificationExtension::PushNotification.config[:gcm_api_key]
+          gcm = ::GCM.new(AP::PushNotificationExtension::PushNotification.config[:gcm_api_key])
+          if android_device_tokens
+            gcm_result = gcm.send_notification(android_device_tokens, data: hashed_message_payload)
+            Rails.logger.info "Channel #{self.name}: GCM push status: #{gcm_result}"
+          else
+            Rails.logger.info "Channel #{self.name}: There are no device tokens available for GCM."
           end
         end
-        
-        ios_notifications.each do |ios_notification|
-          APNS.send_notifications([ios_notification])
-        end
-        
-        self.messages << Message.new(alert: alert, badge: badge, message_payload: message_payload)
-      else
-        Rails.logger.info "Notifications will only be sent out for a production environment."
       end
+      
+      ios_notifications.each do |ios_notification|
+        APNS.send_notifications([ios_notification])
+      end
+      
+      self.messages << Message.new(alert: alert, badge: badge, message_payload: message_payload)
     end
-
   end
 end
